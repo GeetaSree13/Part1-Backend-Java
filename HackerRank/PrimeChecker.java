0a04bd3590dba5f2496e4e6a117f18c8bb3b0fa4
GeetaSree13 <117423480+GeetaSree13@users.noreply.github.com>
2024-11-10 12:16:12 +0530

    Create PrimeChecker.java

---
+++ HackerRank/PrimeChecker.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.Method;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            InputStream in = System.in;
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);    
+            
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            
+            for (Method method : methods) {
+                if (set.contains(method.getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(method.getName());
+            }
+            
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class Prime {
+    public void checkPrime(int... numbers) {
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println();
+    }
+    
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
